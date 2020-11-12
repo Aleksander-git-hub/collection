0e98ac8897d40ef14defad1aade31f8c5cd78dba
Aleksander-git-hub <aleksander.chugunoff@yandex.ru>
2020-11-12 21:06:26 +0300

    Init7

--- src/edu/javacourse/collection/comparable/Example2.java
+++ src/edu/javacourse/collection/comparable/Example2.java
@@ -7,12 +7,13 @@ import java.util.TreeMap;
 public class Example2
 {
     public static void main(String[] args) {
-        User user1 = new User("Jhon", 34, 3000);
-        User user2 = new User("Mark", 26, 3200);
+        User user1 = new User("Jhon", 25, 4000);
+        User user2 = new User("Mark", 25, 3200);
 
         Comparator<User> userComparator = new UserComparator();
+        Comparator<User> ageComparator = new UserAgeComparator();
 
-        Map<User, String> map = new TreeMap<>(userComparator);
+        Map<User, String> map = new TreeMap<>(userComparator.thenComparing(ageComparator));
         map.put(user1, "text1");
         map.put(user2, "text2");
 
---
+++ src/edu/javacourse/collection/comparable/UserAgeComparator.java
@@ -0,0 +1,18 @@
+package edu.javacourse.collection.comparable;
+
+import java.util.Comparator;
+
+public class UserAgeComparator implements Comparator<User>
+{
+
+    @Override
+    public int compare(User o1, User o2) {
+        if (o1.getAge() == o2.getAge()) {
+            return 0;
+        } else if (o1.getAge() < o2.getAge()) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
